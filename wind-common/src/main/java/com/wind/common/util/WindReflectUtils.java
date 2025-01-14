30cf9f5a32860e8964745050c7844f13ee856838
wuxp <1109695647@qq.com>
2025-01-14 14:39:29 +0800

    feature: WindReflectUtils 增加获取 methods 支持

--- wind-common/src/main/java/com/wind/common/util/WindReflectUtils.java
+++ wind-common/src/main/java/com/wind/common/util/WindReflectUtils.java
@@ -9,6 +9,7 @@ import org.springframework.util.ObjectUtils;
 import javax.validation.constraints.NotNull;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -106,6 +108,51 @@ public final class WindReflectUtils {
                 .stream()
                 .map(Field::getName)
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * 获取类的 方法列表
+     *
+     * @param clazz 类类型
+     * @return 方法列表
+     */
+    public static Method[] getMethods(Class<?> clazz) {
+        AssertUtils.notNull(clazz, "argument clazz must not null");
+        return clazz.getMethods();
+    }
+
+    /**
+     * 获取类的公有 getter 方法列表
+     *
+     * @param clazz 类类型
+     * @return 方法列表
+     */
+    public static Method[] getGetterMethods(Class<?> clazz) {
+        AssertUtils.notNull(clazz, "argument clazz must not null");
+        return Arrays.stream(getMethods(clazz))
+                .filter(WindReflectUtils::isGetterMethod)
+                .toArray(Method[]::new);
+    }
+
+    private static boolean isGetterMethod(Method method) {
+        String name = method.getName();
+        if (Objects.equals(name, "getClass")) {
+            // getClass 是 object 中的方法，忽略
+            return false;
+        }
+        // 1. 方法名必须以 "get" 开头，长度 > 3，或以 "is" 开头且长度 > 2
+        boolean isGet = name.startsWith("get") && name.length() > 3;
+        boolean isBooleanGet = name.startsWith("is") && name.length() > 2;
+        // 2. 方法必须没有参数
+        boolean noParams = method.getParameterCount() == 0;
+        // 3. 方法返回类型不能是 void
+        boolean hasReturn = method.getReturnType() != void.class;
+        // 4. 非静态方法
+        boolean isNotStatic = !Modifier.isStatic(method.getModifiers());
+        // 5. 公有方法
+        boolean isPublic = Modifier.isPublic(method.getModifiers());
+        // 综合判断
+        return isPublic && isNotStatic && noParams && hasReturn && (isGet || isBooleanGet);
     }
 
     /**
