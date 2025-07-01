5d19b4f70a30864cf2e4eec840ef9259e3a115d1
wuxp <1109695647@qq.com>
2025-07-01 15:23:55 +0800

    refactor(websocket): 重构会话管理逻辑
    - 将 WindSocketSessionManager 接口扩展，加入会话状态操作接口
    - 新增 WindSocketSessionStatusOperations接口，用于定义会话状态操作
    - 修改会话状态枚举，将 DISABLED 改为 SUSPENDED，更准确地反映挂起状态
    - 移除 WindSocketSessionManager 中的 destroySession

---
+++ wind-websocket/src/main/java/com/wind/websocket/core/WindSocketSessionStatusOperations.java
@@ -0,0 +1,34 @@
+package com.wind.websocket.core;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * 会话状态操作
+ *
+ * @author wuxp
+ * @date 2025-07-01 14:32
+ **/
+public interface WindSocketSessionStatusOperations {
+
+    /**
+     * 激活会话
+     *
+     * @param sessionId 会话 id
+     */
+    void activeSession(@NotBlank String sessionId);
+
+    /**
+     * 挂起一个会话
+     *
+     * @param sessionId 会话 id
+     */
+    void suspendSession(@NotBlank String sessionId);
+
+    /**
+     * 销毁一个会话
+     *
+     * @param sessionId 会话 id
+     */
+    void destroySession(@NotBlank String sessionId);
+
+}
