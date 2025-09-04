0266609828ec65e0bc50bbac8971548a02d86afa
wuxp <1109695647@qq.com>
2025-09-04 17:50:53 +0800

    refactor(websocket): 为 WebSocket 元数据名称添加注释- 在 WindWebSocketMetadataNames 类中，为所有的元数据名称常量添加了中文注释 - 注释解释了每个元数据名称的含义，提高了代码的可读性和维护性

--- wind-websocket/src/main/java/com/wind/websocket/WindWebSocketMetadataNames.java
+++ wind-websocket/src/main/java/com/wind/websocket/WindWebSocketMetadataNames.java
@@ -12,14 +12,30 @@ public final class WindWebSocketMetadataNames {
         throw new AssertionError();
     }
 
+    /**
+     * 客户端id
+     */
     public static final String CLIENT_ID_NAME = "clientId";
 
+    /**
+     * 客户端设备类型
+     */
     public static final String CLIENT_DEVICE_TYPE_NAME = "clientDeviceType";
 
+    /**
+     * 客户端ip
+     *
+     */
     public static final String CLIENT_IP_NAME = "clientIp";
 
+    /**
+     * 用户id
+     */
     public static final String USER_ID_NAME = "userId";
 
+    /**
+     * 用户代理
+     */
     public static final String USER_AGENT_NAME = "userAgent";
 
     /**
