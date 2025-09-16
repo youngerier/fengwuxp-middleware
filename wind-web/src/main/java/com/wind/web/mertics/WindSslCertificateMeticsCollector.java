5be88b62b44bad281b18f28c3dbe2ef6023b7996
wuxp <1109695647@qq.com>
2025-09-16 15:51:22 +0800

    refactor(web): 重构 SSL证书信息收集逻辑
    - 新增 HostSslCertificateInfo 类封装证书信息
    - 重构 collect 方法，使用新方法 getHostSslCertificateInfo 获取证书信息
    - 优化异常处理，使用自定义异常抛出错误
    - 添加测试用例验证新方法

--- wind-web/src/main/java/com/wind/web/mertics/WindSslCertificateMeticsCollector.java
+++ wind-web/src/main/java/com/wind/web/mertics/WindSslCertificateMeticsCollector.java
@@ -1,8 +1,11 @@
 package com.wind.web.mertics;
 
 import com.wind.common.WindConstants;
+import com.wind.common.exception.BaseException;
+import com.wind.common.exception.DefaultExceptionCode;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.net.ssl.SSLSession;
@@ -10,9 +13,10 @@ import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.security.cert.X509Certificate;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Collection;
-import java.util.function.Supplier;
 
 /**
  * ssl 证书信息指标采集
@@ -21,7 +25,7 @@ import java.util.function.Supplier;
  * @date 2025-09-15 16:54
  **/
 @Slf4j
-public record WindSslCertificateMeticsCollector(MeterRegistry meterRegistry, Supplier<Collection<String>> hostsSupplier) {
+public record WindSslCertificateMeticsCollector() {
 
     private static final String DEFAULT_PORT = "443";
 
@@ -37,42 +41,50 @@ public record WindSslCertificateMeticsCollector(MeterRegistry meterRegistry, Sup
 
     private static final String METRIC_DESCRIPTION = "SSL certificate remaining valid days";
 
+    @NotNull
+    public HostSslCertificateInfo getHostSslCertificateInfo(String host) {
+        try {
+            SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            String[] parts = parseHost(host);
+            try (SSLSocket socket = (SSLSocket) factory.createSocket(parts[0], Integer.parseInt(parts[1]))) {
+                socket.startHandshake();
+                SSLSession session = socket.getSession();
+                X509Certificate cert = (X509Certificate) session.getPeerCertificates()[0];
+                Instant notAfter = cert.getNotAfter().toInstant();
+                long availableDays = Instant.now().until(notAfter, ChronoUnit.DAYS);
+                String status = availableDays > 0 ? "VALID" : "EXPIRED";
+                return new HostSslCertificateInfo(host, cert.getSubjectX500Principal().getName(), cert.getIssuerX500Principal().getName(), status, (int) availableDays,
+                        cert.getNotBefore().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(),
+                        notAfter.atZone(ZoneId.systemDefault()).toLocalDateTime());
+            }
+        } catch (Exception exception) {
+            log.warn("Domain ssl metric collect error, host = {}, message = {}", host, exception.getMessage());
+            throw new BaseException(DefaultExceptionCode.COMMON_ERROR, "get host ssl certificate info error", exception);
+        }
+    }
+
     /**
      * 采集 ssl 证书指标信息
      */
-    public void collect() {
-        Collection<String> hosts = hostsSupplier.get();
+    public void collect(MeterRegistry registry, Collection<String> hosts) {
         for (String host : hosts) {
             try {
-                SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-                String[] parts = parseHost(host);
-                try (SSLSocket socket = (SSLSocket) factory.createSocket(parts[0], Integer.parseInt(parts[1]))) {
-                    socket.startHandshake();
-                    SSLSession session = socket.getSession();
-                    X509Certificate cert = (X509Certificate) session.getPeerCertificates()[0];
-                    Instant notAfter = cert.getNotAfter().toInstant();
-                    long availableDays = Instant.now().until(notAfter, ChronoUnit.DAYS);
-                    String status = availableDays > 0 ? "VALID" : "EXPIRED";
-                    log.info("Domain ssl metric collect, host = {}, subject = {}, status= {}, availableDays = {}", host, cert.getSubjectX500Principal(), status, availableDays);
-                    // 用标签区分域名和状态
-                    Gauge.builder(METRIC_GROUP_NAME, () -> availableDays)
-                            .description(METRIC_DESCRIPTION)
-                            .tags(HOST_TAG_NAME, host,
-                                    SUBJECT_TAG_NAME, cert.getSubjectX500Principal().getName(),
-                                    ISSUER_TAG_NAME, cert.getIssuerX500Principal().getName(),
-                                    STATUS_TAG_NAME, status)
-                            .register(meterRegistry);
-                }
+                HostSslCertificateInfo info = getHostSslCertificateInfo(host);
+                Gauge.builder(METRIC_GROUP_NAME, () -> info.availableDays)
+                        .description(METRIC_DESCRIPTION)
+                        .tags(SUBJECT_TAG_NAME, info.subject(),
+                                HOST_TAG_NAME, info.host(),
+                                STATUS_TAG_NAME, info.status())
+                        .register(registry);
             } catch (Exception exception) {
-                log.warn("Domain ssl metric collect error, host = {}, message = {}", host, exception.getMessage());
                 // 失败时也可以暴露为状态 -1
                 Gauge.builder(METRIC_GROUP_NAME, () -> -1)
                         .description(METRIC_DESCRIPTION)
-                        .tags(HOST_TAG_NAME, host,
-                                SUBJECT_TAG_NAME, WindConstants.UNKNOWN,
+                        .tags(SUBJECT_TAG_NAME, WindConstants.UNKNOWN,
+                                HOST_TAG_NAME, host,
                                 ISSUER_TAG_NAME, WindConstants.UNKNOWN,
-                                STATUS_TAG_NAME, "ERROR")
-                        .register(meterRegistry);
+                                STATUS_TAG_NAME, WindConstants.UNKNOWN)
+                        .register(registry);
             }
         }
     }
@@ -82,5 +94,9 @@ public record WindSslCertificateMeticsCollector(MeterRegistry meterRegistry, Sup
             return host.split(":");
         }
         return new String[]{host, DEFAULT_PORT};
+    }
+
+    public record HostSslCertificateInfo(String host, String subject, String issuer, String status, int availableDays, LocalDateTime beginTime, LocalDateTime endTime) {
+
     }
 }
--- wind-web/src/test/java/com/wind/web/mertics/WindSslCertificateMeticsCollectorTests.java
+++ wind-web/src/test/java/com/wind/web/mertics/WindSslCertificateMeticsCollectorTests.java
@@ -3,6 +3,7 @@ package com.wind.web.mertics;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,17 +13,23 @@ import java.util.List;
  * @author wuxp
  * @date 2025-09-16 09:42
  **/
+@Slf4j
 class WindSslCertificateMeticsCollectorTests {
 
     private final MeterRegistry meterRegistry = new SimpleMeterRegistry();
 
-    private final WindSslCertificateMeticsCollector collector = new WindSslCertificateMeticsCollector(meterRegistry, () -> List.of("www.baidu.com"));
+    private final WindSslCertificateMeticsCollector collector = new WindSslCertificateMeticsCollector();
 
+    @Test
+    void testGetHostSslCertificateInfo(){
+        WindSslCertificateMeticsCollector.HostSslCertificateInfo info = collector.getHostSslCertificateInfo("www.baidu.com");
+        log.info("info = {}", info);
+    }
 
     @Test
     void testCollect() {
-        collector.collect();
+        collector.collect(meterRegistry, List.of("www.baidu.com"));
         List<Meter> meters = meterRegistry.getMeters();
-        Assertions.assertEquals(1,meters.size());
+        Assertions.assertEquals(1, meters.size());
     }
 }
